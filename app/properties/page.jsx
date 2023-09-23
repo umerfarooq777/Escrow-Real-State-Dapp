0874897f31fe129e5f664d865935543a2b9f2cf5
Umer Farooq <umerfarooq03100276796@gmail.com>
2023-09-24 02:41:11 +0500

    Update page.jsx

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Properties = dynamic(()=>import('@/Components/Properties/Properties'))
 // SEO
 export const metadata = {
-    title: 'Realhome: Properties',
+    title: 'Realhome: Properties ',
     description: 'Discover a wide range of exceptional real estate properties at our platform. From luxurious apartments in bustling cities to elegant homes in serene neighborhoods, we offer the perfect choices for your dream living. Explore our extensive property listings, advanced search filters, and expert real estate agents ready to assist you every step of the way. Experience a user-friendly interface that allows seamless navigation and management of your listings. Find your ideal property effortlessly and embark on a remarkable journey towards your dream home.',
 }
 
